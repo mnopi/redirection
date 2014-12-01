5459b21a8e12ba3c19fc46c032f6b4409b9f08ae
Ramón Majasol <rmaja@macbook.local>
2014-12-01 20:17:37 +0100

    modif gitignore y settings_prod

--- redirection/settings_prod.py
+++ redirection/settings_prod.py
@@ -3,13 +3,13 @@ from .settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'redirect',
+        'NAME': 'redirection',
         'USER': 'root',
         'PASSWORD': '1aragon1',
         'HOST': '127.0.0.1',
-        'PORT': '3307',
+        'PORT': '3306',
     }
 }
 
-ALLOWED_HOSTS = ['igoo.co', '104.236.21.5', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['igoo.co', '104.131.71.11', 'localhost', '127.0.0.1']
 DEBUG = TEMPLATE_DEBUG = False
