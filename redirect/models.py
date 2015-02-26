c90079b6665d18f1a5aec2af3bc829b602092fb3
mnopi <ramon@bizeulabs.com>
2015-02-26 20:50:26 +0100

    descomentar redirección

--- redirect/models.py
+++ redirect/models.py
@@ -7,7 +7,8 @@ class Page(models.Model):
     android = models.URLField(max_length=200, null=True, blank=True)
     ios = models.URLField(max_length=200, null=True, blank=True)
     other = models.URLField(max_length=200, null=True, blank=True)
-    card_img = models.ImageField(upload_to='pages_imgs', blank=True, null=True)
+    card_img_file = models.ImageField(upload_to='pages_imgs', blank=True, null=True)
+    card_img_url = models.URLField(max_length=200, blank=True, null=True)
     card_title = models.CharField(max_length=140, blank=True, null=True)
 
     def __unicode__(self):
