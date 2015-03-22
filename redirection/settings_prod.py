5b24905a01124f78f76d6c1284e9cc7203484651
rmaja <tishobueno@gmail.com>
2015-03-22 01:06:16 +0100

    añadido mikios.co a ALLOWED_HOSTS

--- redirection/settings_prod.py
+++ redirection/settings_prod.py
@@ -7,9 +7,10 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': '1aragon1',
         'HOST': '127.0.0.1',
+        # 'HOST': 'igoo.co',
         'PORT': '3306',
     }
 }
 
-ALLOWED_HOSTS = ['igoo.co', '104.131.71.11', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['mikios.co', 'igoo.co', '104.131.71.11', 'localhost', '127.0.0.1']
 DEBUG = TEMPLATE_DEBUG = False
